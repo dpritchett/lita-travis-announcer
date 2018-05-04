19a5dd58b7211c6fde20922db1b27857c16d1eef
Daniel J. Pritchett <dpritchett@gmail.com>
2018-05-04 15:56:44 -0500

    Better testing for the Travis hook parser

--- spec/lita/travis_webhook_spec.rb
+++ spec/lita/travis_webhook_spec.rb
@@ -15,13 +15,46 @@ describe Lita::TravisWebhook do
     expect(subject.working?).to be_truthy
     expect(subject.broken?).to be_falsey
   end
- 
+
   context 'a failed build' do
     let(:input_path) { 'travis_failure' }
 
     it 'should acknowledge when the build failed' do
       expect(subject.working?).to be_falsey
       expect(subject.broken?).to be_truthy
+    end
+  end
+
+  describe ':notification_string' do
+    let(:result) { subject.notification_string }
+
+    it 'should include the build status' do
+      subject.stub(:description) { 'hi mom' }
+      expect(result).to include 'hi mom'
+    end
+
+    it 'should include the repo name' do
+      expect(result).to include 'ruby-bookbot'
+    end
+
+    it 'should include the compare URL for the commit' do
+      expect(result).to include 'https://github.com/dpritchett/ruby-bookbot/compare/632250aff63e...7379b6e92f7d'
+    end
+
+    context 'a bad build' do
+      let(:input_path) { 'travis_failure' }
+
+      it 'should include the compare URL for the commit' do
+        expect(result).to include 'https://github.com/dpritchett/ruby-bookbot/commit/64794d01af72'
+      end
+    end
+
+    context 'malformed input' do
+      let(:input_path) { 'travis_malformed' }
+
+      it 'throws an error when parsing' do
+        expect{ subject.description }.to raise_error JSON::ParserError
+      end
     end
   end
 end
